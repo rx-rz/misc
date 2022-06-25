e0108eb6cba432a75219604301f9bff181be480a
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-06-25 07:15:32 +0100

    added Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='font-bold text-6xl bg-gradient-to-r from-slate-500 to-black text-transparent bg-clip-text'>Hello Everyone</h1>
     </div>
   );
 }
