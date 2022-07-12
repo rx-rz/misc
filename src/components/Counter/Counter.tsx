cd1eeda28034fa1611cc754439a4ad44c6e68382
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-07-12 17:40:25 +0100

    redesigned product page ui

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -7,9 +7,9 @@ export default function Counter() {
     <div>
       <p className='dark:text-white mb-1 sm:text-xl sm:mb-3'>quantity: </p>
       <div className='flex mb-4'>
-        <button onClick={decreaseQuantity} className='border-2 px-2 border-black font-extrabold dark:text-white dark:border-primary sm:px-3 sm:py-1'>-</button>
+        <button onClick={decreaseQuantity} className='border-2 px-2 border-black font-extrabold dark:text-white dark:border-primary sm:px-3 sm:py-1 transition-colors duration-300 hover:bg-primary hover:text-white' >-</button>
         <p className='border-2 px-2 border-black border-l-0 border-r-0 font-medium dark:text-white dark:border-primary sm:px-3 sm:py-1'>{quantity}</p>
-        <button onClick={increaseQuantity} className='border-2 px-2 border-black font-extrabold dark:text-white dark:border-primary sm:px-3 sm:py-1'>+</button>
+        <button onClick={increaseQuantity} className='border-2 px-2 border-black font-extrabold dark:text-white dark:border-primary sm:px-3 sm:py-1 transition-colors duration-300 hover:bg-primary hover:text-white'>+</button>
       </div>
     </div>
   )
--- src/features/product/container/ProductSettings.tsx
+++ src/features/product/container/ProductSettings.tsx
@@ -20,26 +20,26 @@ export default function ProductSettings({ name, price }: ProductSettingsProps) {
   };
 
   return (
-    <div className="md:w-5/12 flex justify-center items-center  md:h-maximalHeight flex-col h-halfHeight ">
-      <div className="w-5/6 lg:w-4/6  flex flex-col ">
-        <h1 className="gradient text-4xl md:text-5xl font-medium py-3 lg:text-6xl w-5/6 m-auto">
+    <div className="md:w-5/12 flex justify-center items-center md:h-maximalHeight flex-col h-halfHeight ">
+      <div className="w-11/12   flex flex-col">
+        <h1 className="text-primary text-4xl md:text-5xl font-medium py-3 lg:text-6xl w-5/6 m-auto">
           {name}
         </h1>
-        <p className="text-2xl font-medium md:text-3xl lg:text-5xl dark:text-white mb-4 w-5/6 m-auto">
+        <p className="text-3xl font-medium md:text-4xl lg:text-5xl dark:text-white mb-4 w-5/6 m-auto">
           ${price}
         </p>
         <div className="w-5/6 m-auto">
           <Counter />
         </div>
       </div>
-      <div className="w-5/6 lg:w-4/6">
+      <div className="w-11/12 ">
         <div className="w-5/6 m-auto">
-        <button
-          className="left-0 my-3  font-extrabold bg-primary  p-1 transition duration-300 hover:bg-secondary hover:text-white w-full sm:text-xl"
-          onClick={() => onAdd(product, quantity)}
-        >
-          ADD TO CART
-        </button>
+          <button
+            className="left-0 my-3  font-extrabold bg-primary  p-1 transition-transform duration-300 text-white  w-3/5 sm:w-2/5 font-AlbertSans hover:-translate-y-2"
+            onClick={() => onAdd(product, quantity)}
+          >
+            ADD TO CART
+          </button>
         </div>
       </div>
 
