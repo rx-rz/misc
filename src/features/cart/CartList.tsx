5a154794f7edc90f114282cd3cff30308879dae4
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-07-31 20:51:31 +0100

    cart mobile bug fix

--- src/features/cart/CartList.tsx
+++ src/features/cart/CartList.tsx
@@ -1,4 +1,3 @@
-import{ useRef } from "react";
 import { Link } from "react-router-dom";
 import {CartItem} from "src/components/CartItem";
 import { useStateContext } from "src/context/countContext";
